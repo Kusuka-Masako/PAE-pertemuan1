da22de8226a395ea00da552a60930830b67220a5
Kusuka-Masako <79459969+Kusuka-Masako@users.noreply.github.com>
2022-02-22 14:22:05 +0700

    commit

---
+++ src/test/java/refreshing/pertemuan1/Pertemuan1ApplicationTests.java
@@ -0,0 +1,13 @@
+package refreshing.pertemuan1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pertemuan1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
